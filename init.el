7f0f9bb23af8ed896b10de856fccf3d0ecd9d083
Gabriel Nagy <gabriel.nagy@puppet.com>
2019-05-23 17:17:40 +0300

    Add some sort of auto-update mechanism

--- init.el
+++ init.el
@@ -47,6 +47,13 @@
 (eval-when-compile
   (require 'use-package))
 
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t
+         auto-package-update-interval 4)
+   (auto-package-update-maybe))
+
 (use-package nov
   :ensure t
   :config
