0d5dc8d3a527a0e48c4b51e01cb273ba57777d57
Gabriel Nagy <gabriel.nagy@puppet.com>
2019-10-24 14:14:14 +0300

    Add github actions

--- test-startup.sh
+++ test-startup.sh
@@ -1,8 +1,13 @@
-#!/bin/sh -e
+#!/bin/sh -xe
 if [ -n "$TRAVIS" ]; then
     # Make it look like this is ~/.emacs.d (needed for Emacs 24.3, at least)
     export HOME=$PWD/..
     ln -s emacs.d ../.emacs.d
+fi
+
+if [ -n "$GITHUB_WORKSPACE" ]; then
+    # Work around the github/checkout action by symlinking the checked out configuration
+    ln -s $GITHUB_WORKSPACE $HOME/.emacs.d
 fi
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
